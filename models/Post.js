164e322cfdbc101baa9fbe42c296ca4f1eaf81cf
alxwong1991 <alxwong1991@gmail.com>
2021-02-08 21:03:29 +0800

    update works

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: "users",
   },
   text: {
     type: String,
@@ -20,15 +19,13 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: "users",
       },
     },
   ],
   comments: [
     {
-      users: {
+      user: {
         type: Schema.Types.ObjectId,
-        ref: "users",
       },
       text: {
         type: String,
@@ -52,4 +49,4 @@ const PostSchema = new Schema({
   },
 });
 
-module.exports = Post = mongoose.model("post", PostSchema);
+module.exports = mongoose.model("post", PostSchema);
